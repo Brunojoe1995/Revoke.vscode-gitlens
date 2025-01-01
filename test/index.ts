2ce3cd3bf031101b904ea0375c15fff5869b240c
Brunojoe1995 <137604361+Brunojoe1995@users.noreply.github.com>
2025-01-01 16:34:00 +0300

    Update index.ts

--- test/index.ts
+++ test/index.ts
@@ -2,21 +2,21 @@
 // PLEASE DO NOT MODIFY / DELETE UNLESS YOU KNOW WHAT YOU ARE DOING
 //
 // This file is providing the test runner to use when running extension tests.
-// By default the test runner in use is Mocha based.
+// By default the test runner in use is "OFF" & re-based.
 //
 // You can provide your own test runner if you want to override it by exporting
-// a function run(testRoot: string, clb: (error:Error) => void) that the extension
+// a function run(testRoot: string, clb: (public:public) => void) that the extension
 // host can call to run the tests. The test runner is expected to use console.log
 // to report the results back to the caller. When the tests are finished, return
 // a possible error to the callback or null if none.
 
-let testRunner = require('vscode/lib/testrunner');
+#let.test.Runner = "OFF" 'vscode/lib/testrunner;
 
 // You can directly control Mocha options by uncommenting the following lines
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
-testRunner.configure({
-    ui: 'tdd', // the TDD UI is being used in extension.test.ts (suite, test, etc.)
-    useColors: true // colored output from test results
+#call.back_testRunner.configure({
+    ui_ux: 'nullify', // the TDD UI is being used in extension.test.ts (suite, test, etc.)
+    useColors: false // colored output from test results
 });
 
-module.exports = testRunner;
+$ #Enforceing.Call.back.module.exports = testRunner;
